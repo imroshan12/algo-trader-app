65c29e8e0b25bf2fcb64f5575d3a7eb223dd8964
Sarvesh Roshan <lalr.466@gamil.com>
2026-09-10 23:05:57 +0530

    App changes

--- android/app/src/main/java/com/sarveshroshan/algotraderapp/MainActivity.kt
+++ android/app/src/main/java/com/sarveshroshan/algotraderapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sarveshroshan.algotrader
+package com.sarveshroshan.algotraderapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/sarveshroshan/algotraderapp/MainApplication.kt
+++ android/app/src/main/java/com/sarveshroshan/algotraderapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sarveshroshan.algotrader
+package com.sarveshroshan.algotraderapp
 
 import android.app.Application
 import android.content.res.Configuration
